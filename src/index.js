962f82c6d193034ce6d2dbaaf93802ba1c4b5259
username <fruttys@gmail.com>
2022-09-20 19:19:07 +0300

    add egzam

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
 
-const app = express(); // express inicializavimas
-app.use(express.json()); // kai daromas post, pareitu JSON formatu
-app.use(cors()); // apsauga
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const { port } = require("./config");
 
 const users = require("./routes/users");
 app.use("/users/", users);
-
-// app.get("/", (req, res) => {
-//   res.send({ message: "Server is running" });
-// });
 
 app.all("*", (req, res) => {
   res.status(404).send({ error: "Page not found" });
